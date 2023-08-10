311c8a74bc3efc67dd4de2de248fc1a9cfa9b05c
Ika1009 <ilija.doncic@yahoo.com>
2023-08-10 16:38:32 +0200

    Extension working for blacklists and shows a popup

--- background.js
+++ background.js
@@ -8,27 +8,37 @@ fetch(chrome.runtime.getURL('blacklist.txt'))
   });
 
 // Listen to tab update events
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo && changeInfo.status === 'complete') {
-        checkTab(tab);
-    }
-});
 
 // Listen to tab creation events
 chrome.tabs.onCreated.addListener(function(tab) {
     // Because the tab object on creation does not include the URL,
     // we have to get it again
     chrome.tabs.get(tab.id, function(tab) {
+        console.log("created!");
         checkTab(tab);
     });
 });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo && changeInfo.status === 'complete') {
+        console.log("updated!");
+        checkTab(tab);
+    }
+});
+
 
 async function checkTab(tab) { // Using async to await GetUserId
     let url = tab.url;
+    // Remove 'https://' or 'http://' prefix
+    url = url.replace(/^https?:\/\//, '');
+    // Remove trailing slash
+    if (url.endsWith('/')) url = url.slice(0, -1);
+
     // Only process tabs with a URL (i.e., not a new tab)
-    if (url !== undefined && url !== 'chrome://newtab/') {
+    if (url !== undefined && !url.startsWith('chrome://')) {
+        console.log("URL: " + url);
         if (blacklist.has(url)) { // Now using the set's has method
-            alert('This site is on our blacklist. Proceed with caution!');
+            console.log("BLACKLISTED");
+            showPopup(tab);
         } 
         else {
             let timestamp = Date.now();
@@ -43,23 +53,23 @@ function sendToServer(url, timestamp, userId) {
     // The implementation will depend on your server setup and the language you are using.
     // Here is a very basic example using the Fetch API:
 
-    fetch('https://your-server.com/api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            url: url,
-            timestamp: timestamp,
-            userId: userId
-            // Add any other data you want to send here
-        })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+    // fetch('https://your-server.com/api', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         url: url,
+    //         timestamp: timestamp,
+    //         userId: userId
+    //         // Add any other data you want to send here
+    //     })
+    // })
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch((error) => {
+    //     console.error('Error:', error);
+    // });
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -113,7 +123,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 function generateUserId() {
     let array = new Uint32Array(4);
-    window.crypto.getRandomValues(array);
+    crypto.getRandomValues(array); // Removed the 'window.' prefix
     let id = '';
     for (let i = 0; i < array.length; i++) {
         id += array[i].toString(16).padStart(8, '0');
@@ -142,6 +152,67 @@ function GetUserId() {
     });
 }
 
+function showPopup(tab) {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: function() {
+        // Create a popup container
+        var popupContainer = document.createElement('div');
+        popupContainer.style.position = 'fixed';
+        popupContainer.style.top = '0';
+        popupContainer.style.right = '0';
+        popupContainer.style.left = '0';
+        popupContainer.style.bottom = '0';
+        popupContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        popupContainer.style.zIndex = '10000';
+        popupContainer.style.display = 'flex';
+        popupContainer.style.alignItems = 'center';
+        popupContainer.style.justifyContent = 'center';
+  
+        // Create a popup content container
+        var popupContent = document.createElement('div');
+        popupContent.style.width = '350px';
+        popupContent.style.height = '120px';
+        popupContent.style.padding = '40px';
+        popupContent.style.backgroundColor = 'white';
+        popupContent.style.textAlign = 'center';
+  
+        // Message container
+        var messageContent = document.createElement('div');
+        messageContent.style.fontSize = '20px';
+        messageContent.style.color = '#333333';
+        messageContent.style.fontFamily = "'Arial', sans-serif";
+        messageContent.innerHTML = 'This site is on our blacklist. Proceed with caution!';
+        popupContent.appendChild(messageContent);
+  
+        // Create a close button
+        var closeButton = document.createElement('button');
+        closeButton.innerHTML = 'Close';
+        closeButton.style.marginTop = '20px';
+        closeButton.style.padding = '10px 20px';
+        closeButton.style.border = 'none';
+        closeButton.style.backgroundColor = '#333';
+        closeButton.style.color = 'white';
+        closeButton.style.cursor = 'pointer';
+        closeButton.style.borderRadius = '5px';
+        closeButton.style.fontSize = '16px';
+        closeButton.style.fontFamily = "'Arial', sans-serif";
+        closeButton.onmouseover = function() { closeButton.style.backgroundColor = '#555'; };
+        closeButton.onmouseout = function() { closeButton.style.backgroundColor = '#333'; };
+        closeButton.onclick = function() {
+          document.body.removeChild(popupContainer);
+        };
+  
+        // Append the content and close button to the container
+        popupContent.appendChild(closeButton);
+        popupContainer.appendChild(popupContent);
+  
+        // Append the popup container to the body
+        document.body.appendChild(popupContainer);
+      }
+    });
+  }
+  
 
 // If you want to connect to a server, you will need to send a request here. 
 // You might use the fetch API, jQuery's $.ajax, or another method.
